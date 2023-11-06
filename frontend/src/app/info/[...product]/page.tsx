8aad708ea2ea82f9e651c9e765b8bb7266ea0057
MateusLyoshka <mateussantosf04@gmail.com>
2023-11-06 16:13:57 -0300

    Cadastro de produto

--- frontend/src/app/info/[...product]/page.tsx
+++ frontend/src/app/info/[...product]/page.tsx
@@ -1,3 +1,5 @@
+import { tenProductList } from "@/utils/types";
+
 export default function InfoPage({
   params,
 }: {
@@ -6,6 +8,7 @@ export default function InfoPage({
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-between px-60 py-24">
       <h1>Info page</h1>
+      <div>{tenProductList[0].id}</div>
       <p>{params.product[0]}</p> + <p>{params.product[1]}</p>
     </main>
   );
