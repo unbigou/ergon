6e24188f2688f9a0177093c7d7c3a2a2a35d84f0
William Wallace <bostawwtr@outlook.com>
2023-11-24 21:10:17 -0300

    feat: review entitie

---
+++ backend/src/entities/review.ts
@@ -0,0 +1,31 @@
+import { IReview } from "../interfaces/IReviewInterface";
+import { createUUID } from "../utils/createUUID";
+
+export class Review{
+    id: IReview['id'];
+    rating: IReview['rating'];
+    comentary: IReview['comentary'];
+    userId: IReview['userId'];
+    createdAt: IReview['createdAt'];
+    updatedAt: IReview['updatedAt'];
+
+    constructor(props: Omit<IReview, 'id'>, id?:string){
+        this.id = id || createUUID();
+        this.rating = props.rating;
+        this.comentary = props.comentary;
+        this.userId = props.userId;
+        this.createdAt = props.createdAt || new Date();
+        this.updatedAt = new Date();
+    }
+
+    toJson(): IReview{
+        return{
+            id: this.id,
+            comentary: this.comentary,
+            rating: this.rating,
+            userId: this.userId,
+            createdAt: this.createdAt,
+            updatedAt: this.updatedAt
+        }
+    }
+}
---
+++ backend/src/interfaces/IReviewInterface.ts
@@ -0,0 +1,8 @@
+export interface IReview{
+    id: string;
+    rating: string;
+    comentary: string;
+    userId: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
