63615194d76749581988729182a79ab5481ce7d1
William Wallace <bostawwtr@outlook.com>
2023-11-24 21:18:14 -0300

    feat: review interfaces

--- backend/src/interfaces/IReviewInterface.ts
+++ backend/src/interfaces/IReviewInterface.ts
@@ -6,3 +6,27 @@ export interface IReview{
     createdAt?: Date;
     updatedAt?: Date;
 }
+
+export interface IReviewCreateRequest{
+    rating: string;
+    comentary: string;
+    userId: string;
+}
+
+export interface IReviewGetRequest{
+    id: string;
+}
+
+export interface IReviewGetByUserRequest{
+    userId: string;
+}
+
+export interface IReviewDeleteRequest{
+    id: string;
+}
+
+export interface IReviewUpdateRequest{
+    id: string;
+    comentary: string;
+    userId: string;
+}
